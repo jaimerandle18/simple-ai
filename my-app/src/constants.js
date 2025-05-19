588a82162ec4c47ecf193dba6b51ca98c115ad13
Juan Pablo Carzolio <jpcarzolio@gmail.com>
2025-05-19 13:07:48 -0300

    Add AUTH_URL to constants & env vars

--- my-app/src/constants.js
+++ my-app/src/constants.js
@@ -2,3 +2,4 @@ export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 export const WEBHOOK_URL = process.env.REACT_APP_WEBHOOK_URL;
 export const POLLING_BASE_URL = process.env.REACT_APP_POLLING_BASE_URL;
 export const FILE_BASE_URL = process.env.REACT_APP_FILE_BASE_URL;
+export const AUTH_URL = process.env.REACT_APP_AUTH_URL;
